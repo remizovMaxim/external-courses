3b6894fc3d217a3157bae970a9c420d4e990efbb
Remizov Maxim <sharkercool96@gmail.com>
2019-04-07 20:23:24 +0300

    add viriables for output

--- src/ex6_js-array-methods/task-01.js
+++ src/ex6_js-array-methods/task-01.js
@@ -3,13 +3,15 @@ EPAM PracticWork #6 task-01
 Remizov Maxim, 2019
 */
 function mySlice(array, begin, end){
-  var newArray = [];
+  var newArray = [],
+	  endLint = end,
+	  beginLint = begin;
   array.forEach(function(item, i){   
-    if(end === undefined) end = array.length;     
-    if(end < 0) end = array.length + end;
-    if(begin === undefined) begin = 0;
-    if(begin < 0) begin = array.length + begin;                        
-    if(i >= begin && i < end) newArray.push(item);          
+    if(endLint === undefined) endLint = array.length;     
+    if(endLint < 0) endLint = array.length + endLint;
+    if(beginLint === undefined) beginLint = 0;
+    if(beginLint < 0) beginLint = array.length + beginLint;                        
+    if(i >= beginLint && i < endLint) newArray.push(item);          
   });
   return newArray;
 }
